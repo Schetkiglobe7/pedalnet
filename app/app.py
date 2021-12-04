ad4c794462a51a8e7194002d775bab567760f9a7
Schetkiglobe7 <antoniopilato20@gmail.com>
2021-12-04 20:14:25 +0100

    creazione webapp

---
+++ app/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import datetime
+import os
+from os.path import join, dirname, realpath
+
+UPLOAD_FOLDER = 'static/audio/'
+ALLOWED_EXTENSIONS =  set(['wav'])
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+def version(folder):
+	return str(datetime.datetime.now())
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+app = Flask(__name__, static_url_path = '/assets', static_folder = 'static', template_folder ='templates')
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route("/")
+def homepage():
+	return render_template("index.html", version= version('assets'))
+
+@app.route("/train")
+def trainpage():
+	return render_template("parts/bodyParts/pages/train.html", version= version('assets'))
+
+@app.route("/upload_train_files", methods=['POST'])
+def upload_files():
+	uploaded_file = request.files['file']
+	if uploaded_file.filename != '' and allowed_file(uploaded_file.filename):
+		filename = secure_filename(uploaded_file.filename)
+		uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'] + filename))
+	return redirect(url_for('homepage'))
+
+# color1: #8e7cc3
+# color2: #6a329f
+
+if __name__ == '__main__':
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+    app.run(debug=True)
